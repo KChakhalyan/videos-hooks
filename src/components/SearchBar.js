f4d3b598a53b27a30715747f47298130146ca6a2
Karen <karenchakhalyan23@gmail.com>
2021-03-13 00:26:58 +0400

    some changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+	state = { term: '' };
+
+	onInputChange = (event) => {
+		this.setState({ term: event.target.value });
+	};
+
+	onFormSubmit = (event) => {
+		event.preventDefault();
+
+		// TODO: Make sure we call
+		// callback from parent component
+	};
+
 	render() {
 		return (
-			<div>
-				<h1>Search Bar</h1>
+			<div className=" search-bar ui segment">
+				<form onSubmit={this.onFormSubmit} className="ui form">
+					<div className="field">
+						<label>Videos Search</label>
+						<input
+							type="text"
+							value={this.state.term}
+							onChange={this.onInputChange}
+							// onChange={(e) => this.setState({ term: e.target.value })}
+							// We can use this callback function directly in TAG attribute
+						/>
+					</div>
+				</form>
 			</div>
 		);
 	}
