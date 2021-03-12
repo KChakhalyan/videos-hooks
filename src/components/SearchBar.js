09a062bd854ae6582d5157265047ee02d91d91c1
Karen <karenchakhalyan23@gmail.com>
2021-03-13 00:09:09 +0400

    some changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 	render() {
-		return <div>Search Bar</div>;
+		return (
+			<div>
+				<h1>Search Bar</h1>
+			</div>
+		);
 	}
 }
 
