c1590228d1a6cc6eca9d60aa6c29d0e2a9c165e1
Karen <karenchakhalyan23@gmail.com>
2021-03-31 22:01:52 +0400

    some fixes

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
 	state = { videos: [], selectedVideo: null };
 
 	componentDidMount() {
-		this.onTermSubmit('Hello');
+		this.onTermSubmit('');
 	}
 
 	onTermSubmit = async (term) => {
